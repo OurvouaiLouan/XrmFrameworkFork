3616776561fbd1cc1c4fdccd057fa89bc0d31334
Aymeric Schneider-Maunoury <a.schneider-maunoury@dimsi.fr>
2024-04-11 10:42:44 +0200

    [ WebRss Analyzer ] Setting up the project

---
+++ src/XrmFramework.WebresourcesAnalyzers/Tasks/PostBuildTask.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Task = Microsoft.Build.Utilities.Task;
+
+
+namespace XrmFramework.WebresourcesAnalyzers.Tasks;
+
+public class PostBuildTask : Task
+{
+	[Required]
+	public string GeneratedFilesPath { get; set; }
+	
+	public override bool Execute()
+	{
+		throw new NotImplementedException();
+	}
+}
